1bd5dc0fd9b1a374b4f89da2cb3099baaafad1c0
StuartGavidia <stuarta0403@hotmail.com>
2023-05-10 22:07:23 -0700

    Initial Commit

---
+++ PersonalChatBot/PersonalChatBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalChatBotApp.swift
+//  PersonalChatBot
+//
+//  Created by Stuart G on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PersonalChatBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
